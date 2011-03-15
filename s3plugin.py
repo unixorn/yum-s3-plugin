dc7f7d90e65d29cdf1ba9390a7ecadbfb396dbdb
Robert Mela <rmela@robert-mela-macbook-pro.framemedia.com>
2011-03-15 10:34:10 -0400

    Moving all code into single file.  Single file is more convenient to add to YUM plugins dir.

--- s3plugin.py
+++ s3plugin.py
@@ -5,17 +5,116 @@ This plugin provides access to a protected Amazon S3 bucket using Amazon's speci
 REST authentication scheme.
 """
 
+
+
+
+def createBotoGrabber():
+	import boto
+	from urlparse import urlparse
+
+	class BotoGrabber:
+		def __init__(self, awsAccessKey, awsSecretKey, **kwargs):
+			self.s3 = boto.connect_s3(awsAccessKey, awsSecretKey)
+
+		def _key(self, url):
+			url = urlparse(url)
+			bucket_name = url.netloc.split('.')[0]
+			key_name = url.path[1:]
+			bucket = self.s3.get_bucket(bucket_name)
+			return bucket.get_key(key_name)
+
+		def urlgrab(self, url, filename=None, **kwargs):
+			"""urlgrab(url) copy the file to the local filesystem"""
+			print 'urlgrab s3'
+			key = self._key(url)
+			if not filename: filename = key.key
+			key.get_contents_to_filename(filename)
+			# zzz - does this return a value or something?
+	
+		def urlopen(self, url, **kwargs):
+			"""urlopen(url) open the remote file and return a file object"""
+			return self._key(url)
+
+		def urlread(self, url, limit=None, **kwargs):
+			"""urlread(url) return the contents of the file as a string"""
+			return self._key(url).read()
+
+	return BotoGrabber
+
+
+
+def createUrllibGrabber():
+
+	import os
+	import sys
+	import urllib2
+	import time, hashlib, hmac, base64
+
+	class UrllibGrabber:
+		@classmethod
+		def s3sign(cls,request, secret_key, key_id, date=None):
+        		date=time.strftime("%a, %d %b %Y %H:%M:%S +0000", date or time.gmtime() )
+        		host = request.get_host()
+        		bucket = host.split('.')[0]
+        		request.add_header( 'Date', date)
+        		resource = "/%s%s" % ( bucket, request.get_selector() )
+        		sigstring = """%(method)s\n\n\n%(date)s\n%(canon_amzn_resource)s""" % {
+			                               'method':request.get_method(),
+			                               #'content_md5':'',
+			                               #'content_type':'', # only for PUT
+			                               'date':request.headers.get('Date'),
+			                               #'canon_amzn_headers':'',
+			                               'canon_amzn_resource':resource }
+        		digest = hmac.new(secret_key, sigstring, hashlib.sha1 ).digest()
+        		digest = base64.b64encode(digest)
+        		request.add_header('Authorization', "AWS %s:%s" % ( key_id,  digest ))
+
+		def __init__(self, awsAccessKey, awsSecretKey, **kwargs):
+			self.awsAccessKey = awsAccessKey
+			self.awsSecretKey = awsSecretKey
+
+		def _request(self,url):
+			req = urllib2.Request(url)
+			UrllibGrabber.s3sign(req, self.awsSecretKey, self.awsAccessKey )
+			return req
+
+		def urlgrab(self, url, filename=None, **kwargs):
+			"""urlgrab(url) copy the file to the local filesystem"""
+			req = self._request(url)
+			if not filename:
+				filename = req.get_selector()
+				if filename[0] == '/': filename = filename[1:]
+			out = open(filename, 'w+')
+			resp = urllib2.urlopen(req)
+			buff = resp.read(8192)
+			while buff:
+				out.write(buff)
+				buff = resp.read(8192)
+			# zzz - does this return a value or something?
+	
+		def urlopen(self, url, **kwargs):
+			"""urlopen(url) open the remote file and return a file object"""
+			return urllib2.urlopen( self._request(url) )
+
+		def urlread(self, url, limit=None, **kwargs):
+			"""urlread(url) return the contents of the file as a string"""
+			return urllib2.urlopen( self._request(url) ).read()
+
+	return UrllibGrabber
+
+
+def createGrabber():
+	try: return createBotoGrabber()
+	except: return createUrllibGrabber()
+
+AmazonS3Grabber = createGrabber()
+
 import os
 import sys
 import urllib
-
 from yum.plugins import TYPE_CORE
 from yum.yumRepo import YumRepository
-
 import yum.Errors
-
-from urlgrabber.grabber import URLGrabber
-from urlgrabber.grabber import URLGrabError
 
 __revision__ = "1.0.0"
 
@@ -23,59 +122,40 @@ requires_api_version = '2.5'
 plugin_type = TYPE_CORE
 
 def config_hook(conduit):
-    config.RepoConf.s3_enabled = config.BoolOption(False)
+	config.RepoConf.s3_enabled = config.BoolOption(False)
 
 def init_hook(conduit):
-    """ 
-    Plugin initialization hook. Setup the S3 repositories.
-    """
+	""" 
+	Plugin initialization hook. Setup the S3 repositories.
+	"""
    
-    repos = conduit.getRepos()
-    for i in repos.repos.keys():
-        rs = repos.repos[i]
-        if isinstance(repo, YumRepository) and repo.s3_enabled:
-            new_repo = AmazonS3Repo(idx)
-            new_repo.baseurl = repo.baseurl
-            new_repo.mirrorlist = repo.mirrorlist
-            new_repo.basecachedir = repo.basecachedir
-            new_repo.gpgcheck = repo.gpgcheck
-            new_repo.proxy = repo.proxy
-            new_repo.enablegroups = repo.enablegroups
-                                                                        
-            del rs.repos[repo.id]
-            rs.add(new_repo)
-
-class AmazonS3Grabber:
-    def __init__(self, grabber, awsAccessKey, awsSecretKey, **kwargs):
-        print 'init s3'
-        self.grabber = grabber
-        self.awsAccessKey = awsAccessKey
-        self.awsSecretKey = awsSecretKey
-
-    def urlgrab(self, url, filename=None, **kwargs):
-        print 'urlgrab s3'
-        return self.grabber.urlgrab(url, filename, kwargs)
-    
-    def urlopen(self, url, **kwargs):
-        print 'urlopen s3'
-        return self.grabber.urlopen(url, kwargs)
-
-    def urlread(self, url, limit=None, **kwargs):
-        print 'urlread s3'
-        return self.grabber.urlread(url, limit, kwargs)
+	repos = conduit.getRepos()
+	for i in repos.repos.keys():
+		rs = repos.repos[i]
+		if isinstance(repo, YumRepository) and repo.s3_enabled:
+			new_repo = AmazonS3Repo(idx)
+			new_repo.baseurl = repo.baseurl
+			new_repo.mirrorlist = repo.mirrorlist
+			new_repo.basecachedir = repo.basecachedir
+			new_repo.gpgcheck = repo.gpgcheck
+			new_repo.proxy = repo.proxy
+			new_repo.enablegroups = repo.enablegroups
+																		
+			del rs.repos[repo.id]
+			rs.add(new_repo)
 
 
 class AmazonS3Repo(YumRepository):
-    """
-    Repository object for Amazon S3.
-    """
-    
-    def __init__(self, repoid):
-        YumRepository.__init__(self, repoid)
-        self.enable()
+	"""
+	Repository object for Amazon S3.
+	"""
+	
+	def __init__(self, repoid):
+		YumRepository.__init__(self, repoid)
+		self.enable()
 
-    def _setupGrab(self):
-        YumRepository.setupGrab(self)
-        self.grab = AmazonS3Grabber(self.grab, 'my-access-key', 'my-secret-key')
+	def _setupGrab(self):
+		YumRepository.setupGrab(self)
+		self.grab = AmazonS3Grabber(self.grab, 'my-access-key', 'my-secret-key')
 
-    setupGrab = _setupGrab
+	setupGrab = _setupGrab
--- test/signing.py
+++ test/signing.py
@@ -1,32 +1,38 @@
 import sys, urllib2, time
 sys.path.append('..')
 sys.path.append('.')
-from s3sign import  s3sign
+import s3plugin
 import unittest
+import testvalues
 
 class TestSigning(unittest.TestCase):
 	"""You need to configure SECRET_KEY, KEY_ID, and TEST_URL with real values for your s3 store"""
 
-	SECRET_KEY = 'my_amazon_secret_key'
-	KEY_ID= 'my_amazon_key_id'
-	TEST_URL='http://your_bucket_name.s3.amazonws.com/yourfile.ext'
+	SECRET_KEY = testvalues.SECRET_KEY
+	KEY_ID     = testvalues.KEY_ID
+	TEST_URL   = testvalues.TEST_URL
 
-	def test_invalidkey(self):
-		self.failIf(self.SECRET_KEY=='my_amazon_secret_key')
-		self.failIf(len(self.KEY_ID) != len('0PN5J17HBGZHT7JJ3X82'))
+	def setUp(self):
+		print "Setup"
+		self.grabber = s3plugin.createUrllibGrabber()
+
+	def test_01_constructor(self):
+		grabber = s3plugin.createUrllibGrabber()
+		
+	def test_03_invalidkey(self):
 		req=urllib2.Request(self.TEST_URL)
-		s3sign(req,self.SECRET_KEY, self.KEY_ID)
+		self.grabber.s3sign(req,'bogus_key', self.KEY_ID)
 		self.assertRaises(urllib2.HTTPError, urllib2.urlopen, req )
 
-	def test_urlopen(self):
+	def test_02_urlopen(self):
 		self.failIf(self.SECRET_KEY=='my_amazon_secret_key')
 		self.failIf(len(self.KEY_ID) != len('0PN5J17HBGZHT7JJ3X82'))
 		req=urllib2.Request(self.TEST_URL)
-		s3sign(req,self.SECRET_KEY, self.KEY_ID)
+		self.grabber.s3sign(req,self.SECRET_KEY, self.KEY_ID)
 		resp=urllib2.urlopen(req)
 		self.assertEquals(1,1)
 
-	def test_example_key(self):
+	def test_02_example_key(self):
 
 		"""Validating against example data from
 		http://docs.amazonwebservices.com/AmazonS3/latest/dev/index.html?RESTAuthentication.html"""
@@ -37,7 +43,7 @@ class TestSigning(unittest.TestCase):
 		EXAMPLE_DATE= time.strptime("2007-03-27 19:36:42", "%Y-%m-%d %H:%M:%S")
 
 		req = urllib2.Request(EXAMPLE_URL)
-		s3sign(req, EXAMPLE_SECRET_KEY, EXAMPLE_KEY_ID, EXAMPLE_DATE)
+		self.grabber.s3sign(req, EXAMPLE_SECRET_KEY, EXAMPLE_KEY_ID, EXAMPLE_DATE)
 		desired="AWS 0PN5J17HBGZHT7JJ3X82:xXjDGYUmKxnwqr5KXNPGldn5LbA=" 
 		actual=req.headers.get('Authorization')
 		print "Test1 Desired: %s" % desired
