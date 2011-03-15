07495820d16ff411b00ea13921afd58e5451f24a
Robert Mela <rmela@robert-mela-macbook-pro.framemedia.com>
2011-03-15 10:48:52 -0400

    test/botograbber rewritten to work with new single s3plugin.py file

--- test/botograbber.py
+++ test/botograbber.py
@@ -2,7 +2,7 @@ import sys, unittest, os, os.path
 from urlparse import urlparse
 sys.path.append('..')
 sys.path.append('.')
-from s3_boto_grabber import AmazonS3Grabber
+import s3plugin
 
 import testvalues
 
@@ -16,26 +16,26 @@ class TestBotoGrabber(unittest.TestCase):
 
 
 	TEST_PATH=urlparse(TEST_URL).path.split('/')[-1]
+	def setUp(self):
+		self.grabber = s3plugin.createBotoGrabber()(KEY_ID, SECRET_KEY)
+
 	@classmethod
 	def removeFile(cls):
 		if not cls.TEST_PATH: return
 		if os.path.exists(cls.TEST_PATH): os.unlink(cls.TEST_PATH)
 
 	def test_urlopen(self):
-		grabber = AmazonS3Grabber(KEY_ID, SECRET_KEY)
-		result = grabber.urlopen(TEST_URL)
+		result = self.grabber.urlopen(TEST_URL)
 		data = result.read()
 		self.assertTrue(len(data)>0)
 
 	def test_urlread(self):
-		grabber = AmazonS3Grabber(KEY_ID, SECRET_KEY)
-		result = grabber.urlread(TEST_URL)
+		result = self.grabber.urlread(TEST_URL)
 		self.assertTrue( str == type(result) )
 
 	def test_urlgrab(self):
 		url = urlparse(TEST_URL)
-		grabber = AmazonS3Grabber(KEY_ID, SECRET_KEY)
-		result = grabber.urlgrab(TEST_URL)
+		result = self.grabber.urlgrab(TEST_URL)
 		self.assertTrue(os.path.exists(self.TEST_PATH))
 
 if __name__ == '__main__':
