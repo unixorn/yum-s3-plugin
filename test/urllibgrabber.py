9ef0b6486c4a73f3029ca3206986b43ead26ea93
Robert Mela <rmela@robert-mela-macbook-pro.local>
2011-03-15 22:26:50 -0400

    fixing broken tests

--- test/urllibgrabber.py
+++ test/urllibgrabber.py
@@ -21,41 +21,41 @@ import sys, unittest, os, os.path
 from urlparse import urlparse
 sys.path.append('..')
 sys.path.append('.')
-import s3plugin
+import s3
 
 import testvalues
 
-SECRET_KEY = testvalues.SECRET_KEY
-KEY_ID     = testvalues.KEY_ID
-TEST_URL   = testvalues.TEST_URL
+SECRET_KEY    = testvalues.SECRET_KEY
+KEY_ID        = testvalues.KEY_ID
+TEST_FILE     = testvalues.TEST_FILE
+TEST_URL      = testvalues.TEST_URL
+TEST_URL_BASE = testvalues.TEST_URL_BASE
 
 
 class TestUrlLibGrabber(unittest.TestCase):
 
 
-	TEST_PATH=urlparse(TEST_URL).path.split('/')[-1]
-
 	def setUp(self):
-		self.grabber = s3plugin.createUrllibGrabber()(KEY_ID,SECRET_KEY)
+		self.grabber = s3.createUrllibGrabber()(KEY_ID,SECRET_KEY, baseurl=[TEST_URL_BASE])
 
 	@classmethod
 	def removeFile(cls):
-		if not cls.TEST_PATH: return
-		if os.path.exists(cls.TEST_PATH): os.unlink(cls.TEST_PATH)
+		if not TEST_FILE: return
+		if os.path.exists(TEST_FILE): os.unlink(TEST_FILE)
 
 	def test_urlopen(self):
-		result = self.grabber.urlopen(TEST_URL)
+		result = self.grabber.urlopen(TEST_FILE)
 		data = result.read()
 		self.assertTrue(len(data)>0)
 
 	def test_urlread(self):
-		result = self.grabber.urlread(TEST_URL)
+		result = self.grabber.urlread(TEST_FILE)
 		self.assertTrue( str == type(result) )
 
 	def test_urlgrab(self):
-		url = urlparse(TEST_URL)
-		result = self.grabber.urlgrab(TEST_URL)
-		self.assertTrue(os.path.exists(self.TEST_PATH))
+		url = urlparse(TEST_FILE)
+		result = self.grabber.urlgrab(TEST_FILE)
+		self.assertTrue(os.path.exists(TEST_FILE))
 
 if __name__ == '__main__':
 	unittest.main()
